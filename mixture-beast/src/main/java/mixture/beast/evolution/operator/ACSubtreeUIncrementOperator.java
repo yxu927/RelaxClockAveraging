79fd27b3648139b76d9707066dbcf581de4d61c6
yxu927 <132981793+yxu927@users.noreply.github.com>
2026-03-17 14:31:34 +1300

    ACSubtreeUIncrementOperatortree

--- mixture-beast/src/main/java/mixture/beast/evolution/operator/ACSubtreeUIncrementOperator.java
+++ mixture-beast/src/main/java/mixture/beast/evolution/operator/ACSubtreeUIncrementOperator.java
@@ -8,6 +8,7 @@ import beast.base.inference.Operator;
 import beast.base.inference.parameter.IntegerParameter;
 import beast.base.inference.parameter.RealParameter;
 import beast.base.util.Randomizer;
+import mixture.beast.evolution.util.BranchRateIndexHelper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -43,9 +44,7 @@ public class ACSubtreeUIncrementOperator extends Operator {
     private RealParameter sigma2;
     private RealParameter rootLogRate;
 
-    private int nNodes;
-    private int rootNr;
-    private int[] idxMap;
+    private BranchRateIndexHelper.Mapping mapping;
 
     @Override
     public void initAndValidate() {
@@ -55,47 +54,22 @@ public class ACSubtreeUIncrementOperator extends Operator {
         sigma2 = sigma2Input.get();
         rootLogRate = rootLogRateInput.get();
 
-        if (indicator.getDimension() != 1) throw new IllegalArgumentException("indicator dimension must be 1");
-        if (sigma2.getDimension() != 1) throw new IllegalArgumentException("sigma2 dimension must be 1");
-        if (rootLogRate != null && rootLogRate.getDimension() != 1) throw new IllegalArgumentException("rootLogRate dimension must be 1");
-
-        nNodes = tree.getNodeCount();
-        rootNr = tree.getRoot().getNr();
-
-        if (rates.getDimension() != nNodes - 1) {
-            throw new IllegalArgumentException("rates must have dimension nodeCount-1");
+        if (indicator.getDimension() != 1) {
+            throw new IllegalArgumentException("indicator dimension must be 1");
+        }
+        if (sigma2.getDimension() != 1) {
+            throw new IllegalArgumentException("sigma2 dimension must be 1");
+        }
+        if (rootLogRate != null && rootLogRate.getDimension() != 1) {
+            throw new IllegalArgumentException("rootLogRate dimension must be 1");
         }
 
-        rebuildIdxMap_likeYourPrior();
+        BranchRateIndexHelper.validateRatesDimension(tree, rates, "ACSubtreeUIncrementOperator");
+        mapping = BranchRateIndexHelper.buildDeterministic(tree);
     }
 
-    private void rebuildIdxMap_likeYourPrior() {
-        idxMap = new int[nNodes];
-        Arrays.fill(idxMap, -2);
-
-        int k = 0;
-        for (int i = 0; i < nNodes; i++) {
-            Node node = tree.getNode(i);
-            int nr = node.getNr();
-            if (nr < 0 || nr >= nNodes) throw new IllegalArgumentException("Node nr out of range: " + nr);
-            if (nr == rootNr) idxMap[nr] = -1;
-            else idxMap[nr] = k++;
-        }
-
-        if (k != nNodes - 1) {
-            throw new IllegalStateException("Expected " + (nNodes - 1) + " non-root nodes, got " + k);
-        }
-    }
-
-    private void ensureMapsUpToDate() {
-        int newN = tree.getNodeCount();
-        int newRoot = tree.getRoot().getNr();
-        if (newN != nNodes || newRoot != rootNr) {
-            nNodes = newN;
-            rootNr = newRoot;
-            if (rates.getDimension() != nNodes - 1) throw new IllegalStateException("rates dimension inconsistent with tree");
-            rebuildIdxMap_likeYourPrior();
-        }
+    private void ensureMappingUpToDate() {
+        mapping = BranchRateIndexHelper.ensureUpToDate(tree, mapping, rates, "ACSubtreeUIncrementOperator");
     }
 
     private double rootLog() {
@@ -103,8 +77,10 @@ public class ACSubtreeUIncrementOperator extends Operator {
     }
 
     /** Collect all non-root nodes (edges) in the subtree under node 'n'. */
-    private void collectEdgesInSubtree(Node n, List<Node> out) {
-        if (!n.isRoot()) out.add(n);
+    private void collectEdgesInSubtree(final Node n, final List<Node> out) {
+        if (!n.isRoot()) {
+            out.add(n);
+        }
         final int cc = n.getChildCount();
         for (int i = 0; i < cc; i++) {
             collectEdgesInSubtree(n.getChild(i), out);
@@ -116,24 +92,27 @@ public class ACSubtreeUIncrementOperator extends Operator {
         final boolean allowRoot = allowRootInput.get();
         final int maxEdges = maxSubtreeEdgesInput.get();
         final int tries = maxTriesInput.get();
+        final int nNodes = mapping.getNodeCount();
 
         for (int t = 0; t < tries; t++) {
-            Node cand = tree.getNode(Randomizer.nextInt(nNodes));
-            if (cand.isRoot() && !allowRoot) continue;
-            if (!allowRoot && cand.isRoot()) continue;
+            final Node cand = tree.getNode(Randomizer.nextInt(nNodes));
+            if (cand.isRoot() && !allowRoot) {
+                continue;
+            }
+            if (internalOnly && cand.getChildCount() == 0) {
+                continue;
+            }
 
-            if (internalOnly && cand.getChildCount() == 0) continue;
-
-            // Check subtree size constraint
             if (maxEdges > 0) {
-                ArrayList<Node> tmp = new ArrayList<>();
+                final ArrayList<Node> tmp = new ArrayList<>();
                 collectEdgesInSubtree(cand, tmp);
-                if (tmp.size() > maxEdges) continue;
+                if (tmp.size() > maxEdges) {
+                    continue;
+                }
             }
             return cand;
         }
 
-        // fallback: any non-root node
         Node cand;
         do {
             cand = tree.getNode(Randomizer.nextInt(nNodes));
@@ -142,23 +121,31 @@ public class ACSubtreeUIncrementOperator extends Operator {
     }
 
     /** Compute old u for a subtree rooted at 'node' given parent log-rate 'logPar'. */
-    private void computeUOld(Node node,
-                             double logPar,
-                             double sig2,
-                             double minDt,
-                             double[] xOldByNr,
-                             double[] uOldByNr) {
+    private void computeUOld(final Node node,
+                             final double logPar,
+                             final double sig2Value,
+                             final double minDt,
+                             final double[] xOldByNr,
+                             final double[] uOldByNr) {
         final int nr = node.getNr();
-        final int idx = idxMap[nr];
-        if (idx < 0) throw new ArithmeticException("Bad idxMap");
+        final int idx = mapping.idxForNode(node);
+        if (idx < 0) {
+            throw new ArithmeticException("Bad mapping");
+        }
 
         final double dt = node.getLength();
-        if (!(dt > minDt)) throw new ArithmeticException("dt too small");
-        final double var = sig2 * dt;
-        if (!(var > 0.0)) throw new ArithmeticException("var<=0");
+        if (!(dt > minDt)) {
+            throw new ArithmeticException("dt too small");
+        }
+        final double var = sig2Value * dt;
+        if (!(var > 0.0)) {
+            throw new ArithmeticException("var<=0");
+        }
 
         final double r = rates.getValue(idx);
-        if (!(r > 0.0)) throw new ArithmeticException("rate<=0");
+        if (!(r > 0.0)) {
+            throw new ArithmeticException("rate<=0");
+        }
         final double x = Math.log(r);
 
         final double mean = logPar - 0.5 * var;
@@ -169,25 +156,31 @@ public class ACSubtreeUIncrementOperator extends Operator {
 
         final int cc = node.getChildCount();
         for (int i = 0; i < cc; i++) {
-            computeUOld(node.getChild(i), x, sig2, minDt, xOldByNr, uOldByNr);
+            computeUOld(node.getChild(i), x, sig2Value, minDt, xOldByNr, uOldByNr);
         }
     }
 
     /** Build new x for subtree rooted at 'node' given parent new log-rate 'logParNew'. */
-    private void buildXNew(Node node,
-                           double logParNew,
-                           double sig2,
-                           double minDt,
-                           double[] uNewByNr,
-                           double[] xNewByNr) {
+    private void buildXNew(final Node node,
+                           final double logParNew,
+                           final double sig2Value,
+                           final double minDt,
+                           final double[] uNewByNr,
+                           final double[] xNewByNr) {
         final int nr = node.getNr();
-        final int idx = idxMap[nr];
-        if (idx < 0) throw new ArithmeticException("Bad idxMap");
+        final int idx = mapping.idxForNode(node);
+        if (idx < 0) {
+            throw new ArithmeticException("Bad mapping");
+        }
 
         final double dt = node.getLength();
-        if (!(dt > minDt)) throw new ArithmeticException("dt too small");
-        final double var = sig2 * dt;
-        if (!(var > 0.0)) throw new ArithmeticException("var<=0");
+        if (!(dt > minDt)) {
+            throw new ArithmeticException("dt too small");
+        }
+        final double var = sig2Value * dt;
+        if (!(var > 0.0)) {
+            throw new ArithmeticException("var<=0");
+        }
 
         final double u = uNewByNr[nr];
         final double x = logParNew - 0.5 * var + Math.sqrt(var) * u;
@@ -196,31 +189,35 @@ public class ACSubtreeUIncrementOperator extends Operator {
 
         final int cc = node.getChildCount();
         for (int i = 0; i < cc; i++) {
-            buildXNew(node.getChild(i), x, sig2, minDt, uNewByNr, xNewByNr);
+            buildXNew(node.getChild(i), x, sig2Value, minDt, uNewByNr, xNewByNr);
         }
     }
 
     @Override
     public double proposal() {
-        ensureMapsUpToDate();
+        ensureMappingUpToDate();
 
         final int k = indicator.getValue(0);
         if (k != 1) {
             return rejectIfNotACInput.get() ? Double.NEGATIVE_INFINITY : 0.0;
         }
 
-        final double sig2 = sigma2.getValue(0);
+        final double sig2Value = sigma2.getValue(0);
         final double minDt = minBranchLengthInput.get();
         final double delta = deltaInput.get();
-        if (!(sig2 > 0.0) || !(minDt > 0.0) || !(delta > 0.0)) return Double.NEGATIVE_INFINITY;
+        if (!(sig2Value > 0.0) || !(minDt > 0.0) || !(delta > 0.0)) {
+            return Double.NEGATIVE_INFINITY;
+        }
 
         final Node subRoot = pickSubtreeRoot();
 
-        // edges (non-root nodes) to update
         final ArrayList<Node> edges = new ArrayList<>();
         collectEdgesInSubtree(subRoot, edges);
-        if (edges.isEmpty()) return Double.NEGATIVE_INFINITY;
+        if (edges.isEmpty()) {
+            return Double.NEGATIVE_INFINITY;
+        }
 
+        final int nNodes = mapping.getNodeCount();
         final double[] xOldByNr = new double[nNodes];
         final double[] uOldByNr = new double[nNodes];
         final double[] uNewByNr = new double[nNodes];
@@ -230,15 +227,13 @@ public class ACSubtreeUIncrementOperator extends Operator {
         Arrays.fill(uNewByNr, Double.NaN);
         Arrays.fill(xNewByNr, Double.NaN);
 
-        // boundary parent log-rate for the subtree root edge
-        double boundaryLogPar;
+        final double boundaryLogPar;
         if (subRoot.isRoot()) {
             boundaryLogPar = rootLog();
-            // root itself has no edge; compute for children
             final int cc = subRoot.getChildCount();
             try {
                 for (int i = 0; i < cc; i++) {
-                    computeUOld(subRoot.getChild(i), boundaryLogPar, sig2, minDt, xOldByNr, uOldByNr);
+                    computeUOld(subRoot.getChild(i), boundaryLogPar, sig2Value, minDt, xOldByNr, uOldByNr);
                 }
             } catch (ArithmeticException bad) {
                 return Double.NEGATIVE_INFINITY;
@@ -248,57 +243,62 @@ public class ACSubtreeUIncrementOperator extends Operator {
             if (parent.isRoot()) {
                 boundaryLogPar = rootLog();
             } else {
-                final int idxPar = idxMap[parent.getNr()];
-                if (idxPar < 0) return Double.NEGATIVE_INFINITY;
+                final int idxPar = mapping.idxForNode(parent);
+                if (idxPar < 0) {
+                    return Double.NEGATIVE_INFINITY;
+                }
+
                 final double rPar = rates.getValue(idxPar);
-                if (!(rPar > 0.0)) return Double.NEGATIVE_INFINITY;
+                if (!(rPar > 0.0)) {
+                    return Double.NEGATIVE_INFINITY;
+                }
                 boundaryLogPar = Math.log(rPar);
             }
+
             try {
-                computeUOld(subRoot, boundaryLogPar, sig2, minDt, xOldByNr, uOldByNr);
+                computeUOld(subRoot, boundaryLogPar, sig2Value, minDt, xOldByNr, uOldByNr);
             } catch (ArithmeticException bad) {
                 return Double.NEGATIVE_INFINITY;
             }
         }
 
-        // Propose new u (RW) for all edges in the chosen subtree
-        for (Node e : edges) {
+        for (final Node e : edges) {
             final int nr = e.getNr();
             final double uOld = uOldByNr[nr];
-            if (!Double.isFinite(uOld)) return Double.NEGATIVE_INFINITY;
+            if (!Double.isFinite(uOld)) {
+                return Double.NEGATIVE_INFINITY;
+            }
             uNewByNr[nr] = uOld + delta * Randomizer.nextGaussian();
         }
 
-        // Rebuild new x from boundary using proposed u
         try {
             if (subRoot.isRoot()) {
                 final int cc = subRoot.getChildCount();
                 final double rootLog = rootLog();
                 for (int i = 0; i < cc; i++) {
-                    buildXNew(subRoot.getChild(i), rootLog, sig2, minDt, uNewByNr, xNewByNr);
+                    buildXNew(subRoot.getChild(i), rootLog, sig2Value, minDt, uNewByNr, xNewByNr);
                 }
             } else {
-                buildXNew(subRoot, boundaryLogPar, sig2, minDt, uNewByNr, xNewByNr);
+                buildXNew(subRoot, boundaryLogPar, sig2Value, minDt, uNewByNr, xNewByNr);
             }
         } catch (ArithmeticException bad) {
             return Double.NEGATIVE_INFINITY;
         }
 
-        // Hastings ratio in rate-space for symmetric RW in u:
-        // logH = sum_{updated edges} (xNew - xOld)
         double sumDelta = 0.0;
-        for (Node e : edges) {
+        for (final Node e : edges) {
             final int nr = e.getNr();
             final double xo = xOldByNr[nr];
             final double xn = xNewByNr[nr];
-            if (!Double.isFinite(xo) || !Double.isFinite(xn)) return Double.NEGATIVE_INFINITY;
+            if (!Double.isFinite(xo) || !Double.isFinite(xn)) {
+                return Double.NEGATIVE_INFINITY;
+            }
             sumDelta += (xn - xo);
         }
 
-        // Commit new rates
         rates.startEditing(this);
-        for (Node e : edges) {
-            final int idx = idxMap[e.getNr()];
+        for (final Node e : edges) {
+            final int idx = mapping.idxForNode(e);
             rates.setValue(idx, Math.exp(xNewByNr[e.getNr()]));
         }
 
